eaf3e7191ffef2d3fa999d2ea0948b457651f7ee
isai.madueno <isai.madueno@stjsonora.gob.mx>
2022-05-15 21:49:33 -0700

    fix error handler

--- utils/handleError.js
+++ utils/handleError.js
@@ -1,13 +1,13 @@
 /**
  * manejador de errores custom
  * @param {*} res
- * @param {*} err
+ * @param {*} err objeto error del catch
  * @param {*} internal string verboso de error
  * @param {*} code http status code
  */
 
 const handleHttpError = (res, err, internal = "", code = 403) => {
-  const { message } = err;
+  const message = err.message ?? "ALGO SALIO MAL";
   res.status(code);
   res.send({
     error: { message, internal },
